61d64c99edf4a535264fa3e25f4e64e0c2adc28e
sii.przemysj <przemyslaw.jopa@arionbanki.is>
2025-09-16 14:28:31 +0200

    Fix claims token url

--- Arion-BusinessApi-SampleClient/Program.cs
+++ Arion-BusinessApi-SampleClient/Program.cs
@@ -14,7 +14,7 @@ class Program
     private static readonly string SANDBOX_CARD_ID = "[Place your sandbox card id here]"; // this is returned from endpoint /api/v1/cards "resourceId"
 
     // Base path to claims API
-    private static readonly string IOBWS_CLAIMS_BASE_PATH = "https://apigw.arionbanki.is/claims/api/v1";
+    private static readonly string IOBWS_CLAIMS_BASE_PATH = "https://apigw-test.arionbanki.is/claims/api/v1";
 
     // Config these variables to your needs
     private static readonly string CLAIM_ID = "[Place the claim id here]"; // this is returned from endpoint /api/v1/claims/{claimId} "resourceId"
@@ -23,7 +23,7 @@ class Program
     private static readonly string DATE_TO = "YYYY-mm-dd"; // this is the date from when records will be taken "YYYY-mm-dd"
 
     // Authorization for Claims API
-    private static readonly string CLAIMS_TOKEN_URL = "https://isit-identity-tokenservice-api-main.prod.service.arionbanki.is/v2/oauth-token"; // Token url to identity service
+    private static readonly string CLAIMS_TOKEN_URL = "https://apigw-test.arionbanki.is/oauth/v2/oauth-token"; // Token url to identity service
     private static readonly string YOUR_CERTIFICATE = "[Place your certificate in here]"; // This is the certificate you will use to authenticate to the identity service
     private static readonly string CLIENT_ID = "[Place your client id here]";
     private static readonly string CLIENT_SECRET = "[Place your client secret here]";
